738fa94fd94c5fbf9b04e3f70beb07c45668aad3
Dominux <kir.gimranov@gmail.com>
2021-12-18 17:09:01 +0300

    Chapter 4: Adding a sphere

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 use std::{fs::File, io::Write};
 
+use vec3::Vec3;
+use ray::Ray;
+
 mod vec3;
 mod ray;
 
 
-fn color(r: ray::Ray) -> vec3::Vec3 {
+#[allow(non_snake_case)]
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool {
+	let oc = r.origin() - *center;
+
+	let a = r.direction().dot(&r.direction());
+	let b = 2.0 * oc.dot(&r.direction());
+	let c = oc.dot(&oc) - radius.powi(2);
+
+	let D = b.powi(2) - 4.0 * a * c;
+	D > 0.0
+}
+
+fn color(r: Ray) -> Vec3 {
+	if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &r) {
+		return Vec3::new(1.0, 0.0, 0.0)
+	}
 	let unit_direction = r.direction().unit_vector();
 	let t = 0.5 * (unit_direction.y() + 1.0);
-	(1.0 - t) * vec3::Vec3::new(1.0, 1.0, 1.0) + t * vec3::Vec3::new(0.5, 0.7, 1.0)
+	(1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
 
 fn main() -> std::io::Result<()> {
@@ -17,17 +35,17 @@ fn main() -> std::io::Result<()> {
 	let mut file = File::create("renders/image.ppm")?;
 	file.write_all(format!("P3\n{} {}\n255\n", NX, NY).as_bytes())?;
 
-	let lower_left_corner = vec3::Vec3::new(-2.0, -1.0, -1.0);
-	let horizontal = vec3::Vec3::new(4.0, 0.0, 0.0);
-	let vertical = vec3::Vec3::new(0.0, 2.0, 0.0);
-	let origin = vec3::Vec3::new(0.0, 0.0, 0.0);
+	let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+	let horizontal = Vec3::new(4.0, 0.0, 0.0);
+	let vertical = Vec3::new(0.0, 2.0, 0.0);
+	let origin = Vec3::new(0.0, 0.0, 0.0);
 
 	for j in (0..NY).rev() {
 		for i in 0..NX { 
 			let u = i as f64 / NX as f64 ;
 			let v = j as f64 / NY as f64;
 
-			let r = ray::Ray::new(&origin, &(lower_left_corner + u * horizontal + v * vertical));
+			let r = Ray::new(&origin, &(lower_left_corner + u * horizontal + v * vertical));
 			let col = color(r);
 
 			let ir = (255.99 * col.r()) as u8;
